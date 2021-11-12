d50d87a8ca6010fd2d7fd2703a9299e29833e431
Sviatoslav Myskiv <79469781+SkVaDaR@users.noreply.github.com>
2021-11-12 11:50:24 +0200

    Add files via upload

---
+++ js/dz1/classwork/script.js
@@ -0,0 +1,57 @@
+//1
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let result = numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5]+numbers[6]+numbers[7]+numbers[8]+numbers[9];
+// console.log(result);
+
+//2
+// let book = {
+//     title: 'Hello World',
+//     amountOfPages: 1,
+//     type: 'learning'
+// }
+
+//3
+// let book1 = {
+//     title: 'Arthur',
+//     amountOfPages: 400,
+//     type: 'fantastic',
+//     author: 'Luke Besson'
+// }
+
+//4
+// let books = [
+//     book1 = {
+//         title: 'Hello World',
+//         amountOfPages: 1,
+//         type: 'learning'
+//     },
+//     book2 = {
+//         title: 'Arthur',
+//         amountOfPages: 400,
+//         type: 'fantastic',
+//         author: 'Luke Besson'
+//     }
+// ];
+// let bookOneOut = books[0];
+// console.log(bookOneOut);
+// let bookTwoOut = books[1];
+// console.log(bookTwoOut);
+
+//5
+// let height = 23;
+// let width = 10;
+// let s = height*width;
+// console.log(s);
+
+//6
+// let heightC = 10;
+// let rC = 2;
+// let pi = 3.14
+// let v = pi*Math.pow(rC,2)*10;
+// console.log(v);
+
+//7
+// let n = 3;
+// let m = 4;
+// let k = Math.sqrt(((Math.pow(3, 2))+(Math.pow(4, 2))), 2);
+// console.log(k);
---
+++ js/dz1/homework/script.js
@@ -0,0 +1,173 @@
+//1
+// let a = 'hello';
+// console.log(a);
+// alert(a);
+// document.write(a);
+// let b = 'owu';
+// console.log(b);
+// alert(b);
+// document.write(b);
+// let c = 'com';
+// console.log(c);
+// alert(c);
+// document.write(c);
+// let d = 'ua';
+// console.log(d);
+// alert(d);
+// document.write(d);
+// let e = 1;
+// console.log(e);
+// alert(e);
+// document.write(e);
+// let f = 10;
+// console.log(f);
+// alert(f);
+// document.write(f);
+// let g = -999;
+// console.log(g);
+// alert(g);
+// document.write(g);
+// let h = 123;
+// console.log(h);
+// alert(h);
+// document.write(h);
+// let i = 3.14;
+// console.log(i);
+// alert(i);
+// document.write(i);
+// let j = 2.7;
+// console.log(j);
+// alert(j);
+// document.write(j);
+// let k = 16;
+// console.log(k);
+// alert(k);
+// document.write(k);
+// let l = true;
+// console.log(l);
+// alert(l);
+// document.write(l);
+// let m = false;
+// console.log(m);
+// alert(m);
+// document.write(m);
+
+//2
+// let a = 'hello';
+// a = 'hi'
+// console.log(a);
+// alert(a);
+// document.write(a);
+// let b = 'owu';
+// b = 'okten'
+// console.log(b);
+// alert(b);
+// document.write(b);
+// let c = 'com';
+// c = 'org'
+// console.log(c);
+// alert(c);
+// document.write(c);
+// let d = 'ua';
+// d = 'uk'
+// console.log(d);
+// alert(d);
+// document.write(d);
+// let e = 1;
+// e = 0;
+// console.log(e);
+// alert(e);
+// document.write(e);
+// let f = 10;
+// f = 9;
+// console.log(f);
+// alert(f);
+// document.write(f);
+// let g = -999;
+// g = -1000;
+// console.log(g);
+// alert(g);
+// document.write(g);
+// let h = 123;
+// h = 321;
+// console.log(h);
+// alert(h);
+// document.write(h);
+// let i = 3.14;
+// i = 'pi';
+// console.log(i);
+// alert(i);
+// document.write(i);
+// let j = 2.7;
+// j = 2.5;
+// console.log(j);
+// alert(j);
+// document.write(j);
+// let k = 16;
+// k = 18;
+// console.log(k);
+// alert(k);
+// document.write(k);
+// let l = true;
+// l = false;
+// console.log(l);
+// alert(l);
+// document.write(l);
+// let m = false;
+// m = true;
+// console.log(m);
+// alert(m);
+// document.write(m);
+
+//3
+// let firstName = 'Sviatoslav';
+// let middleName = 'Olegovych';
+// let lastName = 'Myskiv';
+// let person = `${firstName} ${middleName} ${lastName}`
+// document.write(person);
+
+//4
+// let firstName = prompt(`Введіть своє ім'я`);
+// let middleName = prompt(`Введіть своє по-батькові`);
+// let age = prompt(`Скільки вам років?`);
+// alert(`Вітаю ${firstName} ${middleName}. Тобі ${age} років`)
+
+//5
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+//6
+// let a = 5 < 6; //-> true
+// console.log(a);
+// let b = 5 > 6; //-> false
+// console.log(b);
+// let c = 5 >= 6; //-> false
+// console.log(c);
+// let d = 5 == 6; //-> false
+// console.log(d);
+// let e = 10 == 10; //-> true
+// console.log(e);
+// let f = 10 === 10; //-> true
+// console.log(f);
+// let g = 10 < 10; //-> false
+// console.log(g);
+// let h = 10 > 10; //-> false
+// console.log(h);
+// let i = 10 != 10; //-> false
+// console.log(i);
+// let j = 123 === '123'; //-> false
+// console.log(j);
+// let k = 123 == '123'; //-> true
+// console.log(k);
+
+//7
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");//205 не приводить 20 до числового типу
+// document.write(str - a + "<br/>");//15 намагається відняти 20-5 і приводить 20 до типу number
+// document.write(str * "2" + "<br/>");//40 так само приводить 20 до типу number
+// document.write(str / 2 + "<br/>");//10
